b12adc0e9f2463b8598304a9d00d05d67f0a7322
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-27 00:35:53 +0200

    feat: Добавил i18n
    i18n был добавлен, настроен минимально, а так же используется пример

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,49 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+// @ts-ignore
+import { createI18n } from "vue-i18n";
 
-app.use(router)
+/**
+ * Набор переводов.
+ * Позже разобью по отдельным файлам.
+ */
+const messages = {
+  en: {
+    "hello": "hello world"
+  },
+  ru: {
+    "hello": "Привет"
+  }
+};
 
-app.mount('#app')
+/*
+ * Создаю и настраиваю i18n для мультиязычности
+ */
+const i18n = createI18n({
+  // Будем использовать Composition API
+  legacy: false,
+  // Язык по умолчанию
+  locale: "ru",
+  /* По умолчанию буду использовать английский язык,
+   * если передали ненастроенный язык.
+   */
+  fallbackLocale: "en",
+  /* Автодобавление глобальной переменной $t
+   * во все файлы.
+   */
+  globalInjection: true,
+  // Набор переводов
+  messages
+});
+
+const app = createApp(App);
+
+app.use(i18n);
+
+app.use(router);
+
+app.mount('#app');
