8dd79162da825fe7f8a2ab8f3e27381427fa93fb
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-27 04:57:53 +0200

    feat: Добавлено управление мета-заголовками
    Теперь отображаются МЕТА-заголовки для каждой отдельной страницы

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,29 @@
 import { routes } from '@/menu-elements'
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory,
+  type NavigationGuardNext,
+  type RouteLocationNormalized
+} from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes
-})
+});
+
+/**
+ * При помощи JS обновляем мета-заголовок страницы.
+ */
+router.beforeEach(
+  (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: NavigationGuardNext
+  ): void => {
+    document.title =
+      (to.meta.title ? to.meta.title + " -- " : "") + "Калькулятор качества производства LIF YO.";
+    next();
+  }
+);
 
 export default router
