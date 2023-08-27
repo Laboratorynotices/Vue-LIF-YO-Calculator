d1ce6988d5e1a7ed127cc8a9c4405be8ace335cd
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-27 03:02:53 +0200

    feat: Добавил Vuetify

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,12 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <header>
-    {{
-      // @ts-ignore
-      $t("hello")
-    }}
+    <div class="bg-green pa-2">
+      {{
+        // @ts-ignore
+        $t("hello")
+      }}
+    </div>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,20 @@ import router from './router';
 // Настройка мультиязычности
 import { i18n } from '@/i18n/index';
 
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
 const app = createApp(App);
+
+app.use(vuetify);
 
 app.use(i18n);
 
