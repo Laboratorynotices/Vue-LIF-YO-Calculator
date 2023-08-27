b0edf81de1e5344f676c0be38a0322d32828dfd3
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-27 12:29:38 +0200

    feat: Добавлен каркас
    Использовал один из готовых каркасов Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import { navElements } from '@/menu-elements';
-import DummyComponent from "@/components/DummyComponent.vue";
-
+import LayoutMain from "@/layouts/constrained/LayoutMain.vue";
 </script>
 
 <template>
-  <header>
-    <div class="bg-green pa-2">
-      {{
-        // @ts-ignore
-        $t("hello")
-      }}
-    </div>
-
-    <div class="wrapper">
-      <nav>
-        <router-link
-          v-for="(navElement, i) in navElements"
-          :key="i"
-          :to="navElement.path"
-        >{{ navElement.nameMenu }}</router-link>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-  <dummy-component></dummy-component>
+  <layout-main></layout-main>
 </template>
---
+++ src/layouts/constrained/LayoutMain.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import { navElements } from '@/menu-elements';
+import DummyComponent from "@/components/DummyComponent.vue";
+
+const links = [
+  'Dashboard',
+  'Messages',
+  'Profile',
+  'Updates',
+];
+</script>
+
+<template>
+  <v-app id="inspire">
+    <v-app-bar flat>
+      <v-container class="mx-auto d-flex align-center justify-center">
+        <v-avatar
+          class="me-4 "
+          color="grey-darken-1"
+          size="32"
+        ></v-avatar>
+
+        <router-link
+          v-for="(navElement, i) in navElements"
+          :key="i"
+          :to="navElement.path"
+        >{{ navElement.nameMenu }}</router-link>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          :text="link"
+          variant="text"
+        ></v-btn>
+
+        <v-spacer></v-spacer>
+  
+        <v-responsive max-width="160">
+          <v-text-field
+            density="compact"
+            flat
+            hide-details
+            label="Search"
+            rounded="lg"
+            single-line
+            variant="solo-filled"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  link
+                  :title="`List Item ${n}`"
+                ></v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
+                  color="grey-lighten-4"
+                  link
+                  title="Refresh"
+                ></v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+              <RouterView />
+              <dummy-component></dummy-component>
+              <div class="bg-green pa-2">
+                {{
+                  // @ts-ignore
+                  $t("hello")
+                }}
+              </div>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
